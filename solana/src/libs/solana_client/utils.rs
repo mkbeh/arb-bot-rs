01932924721a841085b5f99d8b11ea492be1721f
mkbeh <gsilents226@gmail.com>
2026-02-05 03:56:30 +0300

    fix: lint

--- solana/src/libs/solana_client/utils.rs
+++ solana/src/libs/solana_client/utils.rs
@@ -1,6 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 /// Computes the current timestamp in milliseconds since the Unix epoch.
+#[must_use]
 #[inline(always)]
 pub fn get_timestamp_ms() -> u64 {
     SystemTime::now()
